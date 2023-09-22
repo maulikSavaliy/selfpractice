384609ec3701d480e90506ddec7126e5c136b202
maulik savaliya <maulikkumar955@gmail.com>
2023-09-22 09:35:55 +0100

    2 commit

--- src/first.java
+++ src/first.java
@@ -5,7 +5,7 @@ public class first {
     public static void main(String[] args) {
         cocatinationstring("maulik","savaliya");
         samesequance("maulik savaliya","savaliya");
-
+        onepractice.spacialchar("today £ rate is 104");
 
 
     }
@@ -21,5 +21,10 @@ public class first {
         System.out.println("String 1 :" + name);
         System.out.println("String 2 :" + surname);
         System.out.println("to find same sequance :" + name.contains(surname));
+    }
+    //remove spacial char
+    void spacialchar(String santance){
+        System.out.println("original santance :" + santance);
+        System.out.println("new santance :" + santance.replaceAll("[^a-zA-Z0-9]"," "));
     }
 }
